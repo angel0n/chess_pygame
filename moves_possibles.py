306f2f4f65f4a3c999d8e90777e1cc0a0346df40
angel0n <63112226+angel0n@users.noreply.github.com>
2025-04-22 19:50:19 -0300

    melhorado a movimentação da torre

--- moves_possibles.py
+++ moves_possibles.py
@@ -27,45 +27,25 @@ def get_moves_rook(board: list[list[str]], start_row: int, start_col: int):
     piece = board[start_row][start_col]
     oponent = "b" if piece[0] == "w" else "w"
     moves = []
-    if(start_row < 7):
-        for row in range(start_row + 1, 8):
-            if(board[row][start_col] == ""):
-               moves.append(str(Move(start_row, start_col, row, start_col, board))) 
-            elif(board[row][start_col][0] == piece[0]):
-               break
-            elif(board[row][start_col][0] == oponent):
-               moves.append(str(Move(start_row, start_col, row, start_col, board))) 
-               break
 
-    if(start_row > 0):
-        for row in range(start_row - 1, -1, -1):
-            if(board[row][start_col] == ""):
-               moves.append(str(Move(start_row, start_col, row, start_col, board)))
-            elif(board[row][start_col][0] == piece[0]):
-               break
-            elif(board[row][start_col][0] == oponent):
-               moves.append(str(Move(start_row, start_col, row, start_col, board))) 
-               break
-                 
-    if(start_col < 7):
-        for col in range(start_col + 1, 8):
-            if(board[start_row][col] == ""):
-               moves.append(str(Move(start_row, start_col, start_row, col, board))) 
-            elif(board[start_row][col][0] == piece[0]):
-               break
-            elif(board[start_row][col][0] == oponent):
-               moves.append(str(Move(start_row, start_col, start_row, col, board))) 
-               break
-             
-    if(start_col > 0):
-        for col in range(start_col - 1, -1, -1):
-            if(board[start_row][col] == ""):
-               moves.append(str(Move(start_row, start_col, start_row, col, board))) 
-            elif(board[start_row][col][0] == piece[0]):
-               break
-            elif(board[start_row][col][0] == oponent):
-               moves.append(str(Move(start_row, start_col, start_row, col, board))) 
-               break
+    directions = [(1,0),(-1,0),(0,1),(0,-1)]
+
+    for row_direction, col_direction in directions:
+        row, col = start_row + row_direction, start_col + col_direction
+        while 0 <= row < 8 and 0 <= col < 8:
+            piece_captured = board[row][col]
+            
+            if(piece_captured == ""):
+                moves.append(str(Move(start_row, start_col, row, col, board)))
+            elif(piece_captured[0] == oponent ):
+                moves.append(str(Move(start_row, start_col, row, col, board)))
+                break
+            elif(piece_captured[0] == piece[0]):
+                break
+
+            row += row_direction
+            col += col_direction 
+            
     return moves
 
 def get_moves_knight(board: list[list[str]], start_row: int, start_col: int):
